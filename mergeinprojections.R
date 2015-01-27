6b131e64a3529d0071d90841cfc7f7ea615893ae
seanasdf <seanasdf@gmail.com>
2015-01-26 19:34:23 -0600

    added more draft picks and replacement level players

--- mergeinprojections.R
+++ mergeinprojections.R
@@ -1,6 +1,21 @@
 batter_positions <- c("C1","C2","1B","2B","SS","3B","CI","MI","OF1","OF2","OF3","OF4","OF5","OF6","DH")
 pitcher_positions <- c("P1","P2","P3","P4","P5","P6","P7","P8","P9","P10")
 
+#read in replacement level hitters
+replacement_hitters <- read.csv("replacement_hitters.csv")
+rownames(replacement_hitters) <- c("C","1B","2B","SS","3B","MI","CI","OF","DH")
+replacement_hitters$AB <- c(400)
+replacement_hitters[c("C1","C2"),] <- replacement_hitters["C",]
+replacement_hitters[c("OF1","OF2","OF3","OF4","OF5","OF6"),] <- replacement_hitters["OF",]
+
+#read in replacement level pitchers
+replacement_pitcher <- c(150,4.47,1.4,4,1,102)
+
+
+#####################################################################
+#############MERGE IN PROJECTIONS FOR EACH PLAYER AND TEAM###########
+#####################################################################
+
 for (team in teams) {
       
       temp <- get(team)
@@ -13,13 +28,14 @@ for (team in teams) {
       hitters <- merge(hitters, hitter_projections, by = "Name", all.x = TRUE)
       pitchers <- merge(pitchers, pitcher_projections, by = "Name", all.x = TRUE)
       
-      #reassign rownames to merged projections
-      rownames(hitters) <- hitters$roster_spot
-      rownames(pitchers) <- pitchers$roster_spot
-            
       #add columns for stats to team
       temp[c("playerid","roster_spot", "AB","R","HR","RBI","SB","AVG","IP","ERA","WHIP","SV","W","K")] <- NA
       
+      #reassign rownames to merged projections
+      rownames(hitters) <- hitters$roster_spot
+      rownames(pitchers) <- pitchers$roster_spot
+      
+
       #add hitter projections to team 
       for (position in batter_positions) {
             temp[position, c("playerid","AB","R","HR","RBI","SB","AVG")] <- 
@@ -31,6 +47,23 @@ for (team in teams) {
             temp[position, c("playerid","IP","ERA","WHIP","SV","W","K")] <- 
                   pitchers[position, c("playerid","IP","ERA","WHIP","SV","W","K")]
       }
+      
+      
+      #add replacement level stats for empty hitter roster spots
+      for (position in batter_positions) {
+            if (temp[position,"Name"] == "") {
+                  temp[position,c("AB","R","HR","RBI","SB","AVG")] <- replacement_hitters[position,c("AB","Runs","HR","RBI","SB","AVG")]
+            }
+      }
+      
+      #add replacement level stats for pitcher spots
+      for (position in pitcher_positions) {
+            if (temp[position,"Name"] == "") {
+                  temp[position,c("IP","ERA","WHIP","SV","W","K")] <- replacement_pitcher
+            }
+      }
+      
+      
       
       assign(team, temp)
       
